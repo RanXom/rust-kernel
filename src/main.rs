0746ebe04dd8081b8680d4af66db38f9331f336c
RanXom <saiyedshizain@gmail.com>
2026-06-09 20:20:09 +0530

    add print macro and panic handler

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,15 @@ mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    print!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"AMAZE! AMAZE! AMAZE!";
-
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-
-    vga_buffer::WRITER.lock().write_str("Hola agayn").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numeros: {} {}", 42, 1.338).unwrap();
+    println!("Hello World{}", "!");
+    panic!("THIS WORKS SOMEHOW!");
 
     loop {}
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -3,6 +3,23 @@ use core::fmt::{self, Write};
 use lazy_static::lazy_static;
 use spin::Mutex;
 
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
+}
+
 impl fmt::Write for Writer {
     fn write_str(&mut self, s: &str) -> fmt::Result {
         self.write_string(s);
